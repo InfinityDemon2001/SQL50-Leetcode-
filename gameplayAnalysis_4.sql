85aef0774e8c75383987f81e8d6b5590394202e1
AllenSabu <94732272+InfinityDemon2001@users.noreply.github.com>
2024-08-23 21:18:07 +0530

    .

---
+++ gameplayAnalysis_4.sql
@@ -0,0 +1,4 @@
+SELECT round(sum(player_login) / count(DISTINCT player_id), 2) AS fraction
+FROM (SELECT player_id, datediff(event_date, min(event_date) 
+OVER (PARTITION BY player_id)) = 1 AS player_login
+FROM activity) AS new_table;
