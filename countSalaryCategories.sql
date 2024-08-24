89badab7fc1b849915caa7af700068e1b7e708e1
AllenSabu <94732272+InfinityDemon2001@users.noreply.github.com>
2024-08-24 12:34:22 +0530

    .

---
+++ countSalaryCategories.sql
@@ -0,0 +1,8 @@
+SELECT "Low Salary" AS category, sum(if(income < 20000, 1, 0)) AS accounts_count
+FROM Accounts
+UNION
+SELECT "Average Salary" AS category, sum(if(income BETWEEN 20000 AND 50000, 1, 0)) AS accounts_count
+FROM Accounts
+UNION
+SELECT "High Salary" AS category, sum(if(income > 50000, 1, 0)) AS accounts_count
+FROM Accounts;
